e57bb6c5d4f5a6a91844bc82cecbcbaf5766eb99
Matt Huisman <me@matthuisman.nz>
2017-06-28 16:38:14 +1200

    Disable scan_always

--- os/Pi_MusicBox/partition_setup.sh
+++ os/Pi_MusicBox/partition_setup.sh
@@ -7,6 +7,7 @@ mkdir -p /tmp/mount
 mount $part1 /tmp/mount
 echo "root=$part2 rootfstype=ext4 elevator=deadline rootwait" > /tmp/mount/cmdline.txt
 sed /tmp/mount/config/settings.ini -i -e "s|resize_once.*|resize_once = false|"
+sed /tmp/mount/config/settings.ini -i -e "s|scan_always.*|scan_always = false|"
 umount /tmp/mount
 sync
 
