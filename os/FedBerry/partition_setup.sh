a145b438eb9073fe6d5ca789a0d687e8fda97a09
Wavetech Matt <me@matthuisman.nz>
2017-09-08 16:37:20 +1200

    Initial Fedberry files

---
+++ os/FedBerry/partition_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$part1" ] || [ -z "$part2" ]; then
+  printf "Error: missing environment variable part1 or part2\n" 1>&2
+  exit 1
+fi
+
+mkdir -p /tmp/1
+mount "$part1" /tmp/1
+sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part1}|"
+umount /tmp/1
