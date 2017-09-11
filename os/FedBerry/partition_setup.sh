8a51b827c0f7bace3ef7dbfdf27d5088a614b1d6
Matt Huisman <me@matthuisman.nz>
2017-09-11 18:55:35 +1200

    Fix partition setup

--- os/FedBerry/partition_setup.sh
+++ os/FedBerry/partition_setup.sh
@@ -11,10 +11,12 @@ mkdir -p /tmp/1
 
 mount "$part1" /tmp/1
 sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
+sed /tmp/1/grub/grub.conf -i -e "s|root=[^ ]*|root=${part2}|"
 umount /tmp/1
 
 mount "$part2" /tmp/1
 sed /tmp/1/etc/fstab -i -e "s|^UUID.* / |$part2  / |"
 sed /tmp/1/etc/fstab -i -e "s|^UUID.* /boot |$part1  /boot |"
+sed /tmp/1/etc/selinux/config -i -e "s|SELINUX=[^ ]*|SELINUX=disabled|"
 rm -r /tmp/1/.rootfs-repartition
 umount /tmp/1
