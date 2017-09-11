e5411982ae1f2edf3ce0ec103943eb29ec0aec0d
Matt Huisman <me@matthuisman.nz>
2017-09-11 17:45:35 +1200

    Update Fedberry

--- os/FedBerry/partition_setup.sh
+++ os/FedBerry/partition_setup.sh
@@ -8,6 +8,13 @@ if [ -z "$part1" ] || [ -z "$part2" ]; then
 fi
 
 mkdir -p /tmp/1
+
 mount "$part1" /tmp/1
-sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part1}|"
+sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
+umount /tmp/1
+
+mount "$part2" /tmp/1
+sed /tmp/1/etc/fstab -i -e "s|^UUID.* / |$part2  / |"
+sed /tmp/1/etc/fstab -i -e "s|^UUID.* /boot |$part1  /boot |"
+rm -r /tmp/1/.rootfs-repartition
 umount /tmp/1
