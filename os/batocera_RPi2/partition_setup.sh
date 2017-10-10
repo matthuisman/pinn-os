42d5c2b8e02fba7771211c51ec7ef6d243205f17
Wavetech Matt <me@matthuisman.nz>
2017-10-10 13:01:09 +1300

    Batocera into individual dirs

---
+++ os/batocera_RPi2/partition_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$part1" ] || [ -z "$part2" ]; then
+  printf "Error: missing environment variable part1 or part2\n" 1>&2
+  exit 1
+fi
+
+mkdir -p /tmp/1
+
+mount "$part1" /tmp/1
+
+sed /tmp/1/cmdline.txt -i -e "s|dev=/dev/[^ ]*|dev=${part1}|"
+
+umount /tmp/1
