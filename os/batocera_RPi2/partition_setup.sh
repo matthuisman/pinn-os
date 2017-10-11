48bcb37937f09bbd6c21bc4b798935931f3ac469
Wavetech Matt <me@matthuisman.nz>
2017-10-11 13:56:08 +1300

    Fix batocera to allow usb root

--- os/batocera_RPi2/partition_setup.sh
+++ os/batocera_RPi2/partition_setup.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-set -ex
+set -x
 
-if [ -z "$part1" ] || [ -z "$part2" ]; then
-  printf "Error: missing environment variable part1 or part2\n" 1>&2
-  exit 1
-fi
+vfat_part=$part1
+ext4_part=$part2
+if [ -n $id1 ]; then vfat_part=$id1; fi
+if [ -n $id2 ]; then ext4_part=$id2; fi
 
 mkdir -p /tmp/1
-
 mount "$part1" /tmp/1
-
-sed /tmp/1/cmdline.txt -i -e "s|dev=[^ ]*|dev=${part1}|"
-
+sed /tmp/1/cmdline.txt -i -e "s|dev=[^ ]*|dev=${vfat_part}|"
 umount /tmp/1
