2f2962881126dfdadc1054284f1e8f4351574bf1
Matt Huisman <me@matthuisman.nz>
2018-01-08 12:53:52 +1300

    Disable Swap

--- DietPi/partition_setup.sh
+++ DietPi/partition_setup.sh
@@ -15,6 +15,7 @@ mount "$part2" /tmp/2
 sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
 sed /tmp/1/dietpi/conf/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/1/dietpi/conf/fstab -i -e "s|^.* /boot |${part1}  /boot |"
+sed /tmp/1/dietpi.txt -i -e "s|Swapfile_Size=1|Swapfile_Size=0|"
 
 sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/2/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
