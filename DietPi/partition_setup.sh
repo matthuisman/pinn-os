ed37e0306193e14f9f122871cb8be1335ac31509
Matt Huisman <me@matthuisman.nz>
2018-01-08 13:39:22 +1300

    Disable resize & enable auto-install

--- DietPi/partition_setup.sh
+++ DietPi/partition_setup.sh
@@ -15,7 +15,9 @@ mount "$part2" /tmp/2
 sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
 sed /tmp/1/dietpi/conf/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/1/dietpi/conf/fstab -i -e "s|^.* /boot |${part1}  /boot |"
+sed /tmp/1/dietpi/boot -i -e "s|FS_Partition$|#FS_Partition|"
 sed /tmp/1/dietpi.txt -i -e "s|Swapfile_Size=1|Swapfile_Size=0|"
+sed /tmp/1/dietpi.txt -i -e "s|AUTO_Install_Enable=0|AUTO_Install_Enable=1|"
 
 sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/2/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
