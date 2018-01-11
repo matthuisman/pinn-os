828d7ec551ba1d33adbee0a5ed6e049dabcb01c2
Matt Huisman <me@matthuisman.nz>
2018-01-11 14:39:48 +1300

    Update kano setup

--- Kano_OS/partition_setup.sh
+++ Kano_OS/partition_setup.sh
@@ -15,6 +15,7 @@ mount "$part2" /tmp/2
 sed /tmp/1/cmdline.txt -i -e "s|root=/dev/[^ ]*|root=${part2}|"
 sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/2/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
+sed /tmp/2/usr/bin/kano-os-loader -i -e "s|/dev/[^ ]*|${part2}|"
 
 umount /tmp/1
 umount /tmp/2
