184d0dfed657bcb3d4f929c7599346827785a8f4
Wavetech Matt <me@matthuisman.nz>
2018-02-21 09:38:22 +1300

    Update Fedberry partition_setup

--- FedBerry/partition_setup.sh
+++ FedBerry/partition_setup.sh
@@ -11,12 +11,12 @@ mkdir -p /tmp/1
 
 mount "$part1" /tmp/1
 sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
+sed /tmp/1/cmdline.txt -i -e "s|logo.nologo|logo.nologo selinux=1 security=selinux enforcing=0|"
 sed /tmp/1/grub/grub.conf -i -e "s|root=[^ ]*|root=${part2}|"
 umount /tmp/1
 
 mount "$part2" /tmp/1
 sed /tmp/1/etc/fstab -i -e "s|^.* / |${part2}  / |"
 sed /tmp/1/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
-sed /tmp/1/etc/selinux/config -i -e "s|SELINUX=[^ ]*|SELINUX=disabled|"
 rm -r /tmp/1/.rootfs-repartition
 umount /tmp/1
