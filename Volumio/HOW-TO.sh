ed5ccccd7957a56d987c30726bbd28ede81af72c
Matt Huisman <me@matthuisman.nz>
2018-04-07 20:40:44 +1200

    Update volumio to allow for ota updates

--- Volumio/HOW-TO.sh
+++ Volumio/HOW-TO.sh
@@ -1,4 +1,4 @@
-# Debian 9 VPS 2 vCore 4096 MB
+# Debian 9 VPS 2 vCore 4096 MB US SERVER
 
 apt-get update && apt-get install -y unzip bsdtar
 
@@ -10,26 +10,28 @@ fdisk -l volumio-*-pi.img
 # Start Sector * Sector Size = Below Offsets
 
 # Boot tarball
-mount -o loop,ro,offset=$((1*512)) volumio-*-pi.img mnt
+mount -o loop,rw,offset=$((1*512)) volumio-*-pi.img mnt
 cd mnt
 bsdtar --numeric-owner --format gnutar -cpf ../boot.tar .
 cd .. && umount mnt
 xz -9 -e boot.tar
 
-# Root tarball
-mount -o loop,ro,offset=$((125001*512)) volumio-*-pi.img mnt
+# volumio tarball
+mount -o loop,rw,offset=$((125001*512)) volumio-*-pi.img mnt
 cd mnt
-bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../root.tar .
+wget https://sourceforge.net/projects/pinn-matthuisman/files/os/Volumio/volumio.initrd
+bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../volumio.tar .
 cd .. && umount mnt
-xz -9 -e root.tar
+xz -9 -e volumio.tar
 
 # Upload tarballs
 sftp matthuisman@frs.sourceforge.net
 cd /home/frs/project/pinn-matthuisman/os/Volumio
 put boot.tar.xz
-put root.tar.xz
+put volumio.tar.xz
+exit
 
 #cleanup
-rm -r mnt && rm boot.tar.xz && rm root.tar.xz && rm volumio-*-pi.img
+rm -r mnt && rm boot.tar.xz && rm volumio.tar.xz && rm volumio-*-pi.img
 
 # UPDATE os.json "version"
--- Volumio/partition_setup.sh
+++ Volumio/partition_setup.sh
@@ -8,46 +8,17 @@ if [ -z "$part1" ] || [ -z "$part2" ] || [ -z "$part3" ]; then
 fi
 
 mkdir -p /tmp/1
-mkdir -p /tmp/3
+mkdir -p /tmp/2
 
 mount "$part1" /tmp/1
-mount "$part3" /tmp/3
+mount "$part2" /tmp/2
 
 sed /tmp/1/cmdline.txt -i -e "s|imgpart=[^ ]*|imgpart=${part2} use_kmsg=no|"
 rm /tmp/1/resize-volumio-datapart
 
-##############################
+rm /tmp/1/volumio.initrd
+cp /tmp/2/volumio.initrd /tmp/1/volumio.initrd
 
-cd /tmp/1
-mv volumio.initrd /tmp/3/volumio.gz
-cd /tmp/3
-gunzip volumio.gz
-mkdir mnt
-cd mnt
-cpio -i -F ../volumio
-
-sed init -i -e "s|/dev/\${BOOTDEV}p1|${part1}|"
-sed init -i -e "s|/dev/\${BOOTDEV}p2|${part2}|"
-sed init -i -e "s|/dev/\${BOOTDEV}p3|${part3}|"
-
-# Make sure resize can't happen
-sed init -i -e "s|/dev/\${BOOTDEV}|/dev/null|"
-sed init -i -e "s|resize-volumio-datapart|i-dont-exist.txt|"
-
-# Update init to update fstab on each boot
-sed init -i -e "/chmod -R 777 \/mnt\/ext\/union\/imgpart/ased \/mnt\/ext\/union\/etc\/fstab -i -e \"s|^/dev.* \/boot |${part1}  \/boot |\""
-
-cpio -i -t -F ../volumio | cpio -o -H newc >../volumio_new
-cd ..
-rm volumio
-mv volumio_new volumio
-gzip volumio
-mv volumio.gz /tmp/1/volumio.initrd
-rm -rf /tmp/3/mnt
-
-##############################
-
-cd /tmp
 sync
 umount /tmp/1
-umount /tmp/3
+umount /tmp/2
