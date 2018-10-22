c9c3d7d89ab3ca89ac0c5877d18291ad971e7670
Matt Huisman <me@matthuisman.nz>
2018-10-23 11:23:36 +1300

    Update Volumio to 2.457

--- Volumio/HOW-TO.sh
+++ Volumio/HOW-TO.sh
@@ -1,9 +1,9 @@
-# Debian 9 VPS 2 vCore 4096 MB Silicon Valley
+# Debian 9 VPS 4 vCore 8192 MB Silicon Valley
 
 apt-get update && apt-get install -y unzip bsdtar aria2
 
 cd ~ && mkdir mnt
-aria2c -x 4 -s 4 http://updates.volumio.org/pi/volumio/2.389/volumio-2.389-2018-03-26-pi.img.zip
+aria2c -x 4 -s 4 http://updates.volumio.org/pi/volumio/2.457/volumio-2.457-2018-09-17-pi.img.zip
 unzip volumio-*-pi.img.zip && rm volumio-*-pi.img.zip
 
 fdisk -l volumio-*-pi.img
@@ -11,20 +11,41 @@ fdisk -l volumio-*-pi.img
 
 # boot tarball
 mount -o loop,rw,offset=$((1*512)) volumio-*-pi.img mnt
-du -h -m --max-depth=0 mnt    #boot uncompressed_tarball_size
-cd mnt
+
+## Make new initrd
+cp mnt/volumio.initrd volumio.initrd
+
+mkdir init && cd init
+zcat ../volumio.initrd | cpio --extract
+rm init
+wget https://raw.githubusercontent.com/matthuisman/pinn-os/master/Volumio/init
+chmod +x init
+find . 2>/dev/null | cpio --quiet --dereference -o -H newc | gzip -9 > ../volumio.initrd
+######
+
+cd ../mnt
+rm volumio.initrd
+cp ../volumio.initrd .
+
 bsdtar --numeric-owner --format gnutar -cpf ../boot.tar .
-cd .. && umount mnt
+cd ..
+rm -r init
+du -h -m --max-depth=0 mnt  #boot uncompressed_tarball_size
+umount mnt
 xz -9 -e boot.tar
 
 # volumio tarball
 mount -o loop,rw,offset=$((125001*512)) volumio-*-pi.img mnt
-du -h -m --max-depth=0 mnt    #volumio uncompressed_tarball_size
 cd mnt
-wget https://sourceforge.net/projects/pinn-matthuisman/files/os/Volumio/volumio.initrd
+cp ../volumio.initrd .
 bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../volumio.tar .
-cd .. && umount mnt
+cd ..
+du -h -m --max-depth=0 mnt    #volumio uncompressed_tarball_size
+umount mnt
 xz -9 -e volumio.tar
+
+# Get total download size in bytes
+echo $(($(wc -c < boot.tar.xz) + $(wc -c < volumio.tar.xz)))
 
 # Upload tarballs
 sftp matthuisman@frs.sourceforge.net
@@ -32,9 +53,6 @@ cd /home/frs/project/pinn-matthuisman/os/Volumio
 put boot.tar.xz
 put volumio.tar.xz
 exit
-
-# Get total download size in bytes
-echo $(($(wc -c < boot.tar.xz) + $(wc -c < volumio.tar.xz)))
 
 # UPDATE os.json
 # UPDATE partitions.json
