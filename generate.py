43c2f3047d9e5e63fa562ca7e19f2268257e0205
Matt Huisman <me@matthuisman.nz>
2018-10-23 11:41:31 +1300

    Update generate.py

--- generate.py
+++ generate.py
@@ -22,11 +22,15 @@ for folder in os.listdir(dname):
         continue
 
     files = os.listdir(folder_path)
-    if "os.json" not in files:
+    if "os.json" not in files and "os_list.json" not in files:
         continue
 
-    with open(os.path.join(folder_path, "os.json"), "r", encoding='utf8') as f:
-        system = json.loads(f.read())
+    system = {}
+
+    os_file = os.path.join(folder_path, "os.json")
+    if os.path.exists(os_file):
+        with open(os.path.join(folder_path, "os.json"), "r", encoding='utf8') as f:
+            system.update(json.loads(f.read()))
 
     partitions_file = os.path.join(folder_path, "partitions.json")
     if os.path.exists(partitions_file):
@@ -37,11 +41,11 @@ for folder in os.listdir(dname):
         for partition in partitions:
             system['nominal_size'] += int(partition.get("partition_size_nominal", 0))
 
-    system['os_name'] = system.pop('name')
+    system['os_name'] = system.pop('name', None)
 
     for key in MAP:
         file_name = MAP[key].format(folder=folder)
-        if file_name in files and key not in system:
+        if file_name in files:
             system[key] = URL_BASE.format(folder=folder, file=file_name)
         # else:
         #     print("{folder} missing {file}".format(folder=folder, file=file_name))
