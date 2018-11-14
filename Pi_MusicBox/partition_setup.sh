60ac82a3d6aa37b075cea4c045d03c31fb8d5924
Matt Huisman <me@matthuisman.nz>
2018-11-15 12:37:37 +1300

    Nicer partition_setup

--- Pi_MusicBox/partition_setup.sh
+++ Pi_MusicBox/partition_setup.sh
@@ -1,17 +1,17 @@
-#!/bin/ash
+#!/bin/sh
 
-set -x
+set -ex
 
-mkdir -p /tmp/mount
+mkdir -p /tmp/1 /tmp/2
 
-mount $part1 /tmp/mount
-echo "root=${part2} rootfstype=ext4 elevator=deadline rootwait" > /tmp/mount/cmdline.txt
-sed /tmp/mount/config/settings.ini -i -e "s|resize_once.*|resize_once = false|"
-sed /tmp/mount/config/settings.ini -i -e "s|scan_always.*|scan_always = false|"
-umount /tmp/mount
-sync
+mount "$part1" /tmp/1
+mount "$part2" /tmp/2
 
-mount $part2 /tmp/mount
-echo "${id1}  /boot  vfat  defaults,user,rw,umask=000  0  2" > /tmp/mount/etc/fstab
-umount /tmp/mount
-sync
+echo "root=${part2} rootfstype=ext4 elevator=deadline rootwait" > /tmp/1/cmdline.txt
+sed /tmp/1/config/settings.ini -i -e "s|resize_once.*|resize_once = false|"
+sed /tmp/1/config/settings.ini -i -e "s|scan_always.*|scan_always = false|"
+
+echo "${id1} /boot vfat defaults,user,rw,umask=000 0 2" > /tmp/2/etc/fstab
+
+umount /tmp/1
+umount /tmp/2
