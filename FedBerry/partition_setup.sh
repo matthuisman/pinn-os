e5657d4ae9baabbb9f9301ce5b03c6841fa08b2b
Matt Huisman <me@matthuisman.nz>
2018-11-15 12:24:20 +1300

    Better partition_setup.sh\

--- FedBerry/partition_setup.sh
+++ FedBerry/partition_setup.sh
@@ -7,16 +7,18 @@ if [ -z "$part1" ] || [ -z "$part2" ]; then
   exit 1
 fi
 
-mkdir -p /tmp/1
+mkdir -p /tmp/1 /tmp/2
 
 mount "$part1" /tmp/1
-sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
-sed /tmp/1/cmdline.txt -i -e "s|logo.nologo|logo.nologo selinux=1 security=selinux enforcing=0|"
-sed /tmp/1/grub/grub.conf -i -e "s|root=[^ ]*|root=${part2}|"
-umount /tmp/1
+mount "$part2" /tmp/2
 
-mount "$part2" /tmp/1
-sed /tmp/1/etc/fstab -i -e "s|^.* / |${part2}  / |"
-sed /tmp/1/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
-rm -r /tmp/1/.rootfs-repartition
+sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
+sed /tmp/1/grub/grub.conf -i -e "s|root=[^ ]*|root=${part2}|"
+sed /tmp/1/cmdline.txt -i -e "s|logo.nologo$|logo.nologo selinux=1 security=selinux enforcing=0|"
+
+sed /tmp/2/etc/fstab -i -e "s|^[^#].* /boot |${part1} /boot |"
+sed /tmp/2/etc/fstab -i -e "s|^[^#].* / |${part2} / |"
+rm -f /tmp/2/.rootfs-repartition
+
 umount /tmp/1
+umount /tmp/2
