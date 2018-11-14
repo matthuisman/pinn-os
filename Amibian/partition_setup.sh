33f0bb1f359b5042ee1e550ec1d22ce09d5ae30c
Matt Huisman <me@matthuisman.nz>
2018-11-15 12:24:56 +1300

    Better partition_setup.sh

--- Amibian/partition_setup.sh
+++ Amibian/partition_setup.sh
@@ -13,8 +13,9 @@ mount "$part1" /tmp/1
 mount "$part2" /tmp/2
 
 sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
-sed /tmp/2/etc/fstab -i -e "s|^.* /boot |${part1}  /boot |"
-sed /tmp/2/etc/fstab -i -e "s|^.* / |${part2}  / |"
+
+sed /tmp/2/etc/fstab -i -e "s|^[^#].* /boot |${part1} /boot |"
+sed /tmp/2/etc/fstab -i -e "s|^[^#].* / |${part2} / |"
 
 umount /tmp/1
 umount /tmp/2
