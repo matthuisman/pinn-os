1b624223a1b6810d2e30e1165aa074415f8cecbe
Matt Huisman <me@matthuisman.nz>
2018-11-15 12:07:32 +1300

    Better partion_setup.sh for DietPi that allows running more than once

--- DietPi/partition_setup.sh
+++ DietPi/partition_setup.sh
@@ -15,11 +15,11 @@ mount "$part2" /tmp/2
 sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
 sed /tmp/1/dietpi.txt -i -e "s|AUTO_SETUP_SWAPFILE_SIZE=1|AUTO_SETUP_SWAPFILE_SIZE=0|"
 
-sed /tmp/2/etc/fstab -i -e "s|^PARTUUID=.* / |${part2}  / |"
-sed /tmp/2/etc/fstab -i -e "s|^PARTUUID=.* /boot |${part1}  /boot |"
-rm /tmp/2/etc/systemd/system/dietpi-fs_expand.service
-rm /tmp/2/var/lib/dietpi/fs_partition_resize.sh
-rm /tmp/2/etc/systemd/system/dietpi-fs_partition_resize.service
+sed /tmp/2/etc/fstab -i -e "s|^[^#].* /boot |${part1} /boot |"
+sed /tmp/2/etc/fstab -i -e "s|^[^#].* / |${part2} / |"
+rm -f /tmp/2/etc/systemd/system/dietpi-fs_expand.service
+rm -f /tmp/2/var/lib/dietpi/fs_partition_resize.sh
+rm -f /tmp/2/etc/systemd/system/dietpi-fs_partition_resize.service
 
 umount /tmp/1
 umount /tmp/2
