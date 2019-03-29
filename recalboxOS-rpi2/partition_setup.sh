1a2d6b93347caae163602da68655bda62ad8677c
Matt Huisman <me@matthuisman.nz>
2019-03-29 13:21:32 +1300

    recalbox 6.0.0 for pi1 and pi2

---
+++ recalboxOS-rpi2/partition_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#supports_backup in PINN
+
+set -ex
+
+if [ -z "$part1" ] || [ -z "$part2" ] || [ -z "$part3" ]; then
+  printf "Error: missing environment variable part1 or part2 or part3\n" 1>&2
+  exit 1
+fi
+
+mkdir -p /tmp/1 /tmp/2
+
+mount "$part1" /tmp/1
+mount "$part2" /tmp/2
+
+sed /tmp/1/cmdline.txt -i -e "s|root=[^ ]*|root=${part2}|"
+
+umount /tmp/1
+umount /tmp/2
