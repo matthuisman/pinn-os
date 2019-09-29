7ab2d7bb360f8c8cf5feb4e31c4fdf7a151df285
Matt Huisman <hi@matthuisman.nz>
2019-09-30 11:34:17 +1300

    DietPi > v6.25

--- DietPi/HOW-TO.sh
+++ DietPi/HOW-TO.sh
@@ -1,16 +1,16 @@
-# Debian 9 VPS 2 vCore 4096 MB Silicon Valley
+# Debian 9 VPS 2 vCore 8192 MB Silicon Valley
 
 apt-get update && apt-get install -y p7zip bsdtar aria2
 
 cd ~ && mkdir mnt
-aria2c -x 4 -s 4  http://dietpi.com/downloads/images/DietPi_RPi-ARMv6-Stretch.7z
-p7zip -d DietPi_RPi-ARMv6-Stretch.7z
+aria2c -x 4 -s 4  https://dietpi.com/downloads/images/DietPi_RPi-ARMv6-Buster.7z
+p7zip -d DietPi_RPi-ARMv6-Buster.7z
 
-fdisk -l DietPi_v*_RPi-ARMv6-Stretch.img
+fdisk -l DietPi_v*_RPi-ARMv6-Buster.img
 # Start Sector * Sector Size = Below Offsets
 
 # boot tarball
-mount -o loop,ro,offset=$((8192*512)) DietPi_v*_RPi-ARMv6-Stretch.img mnt
+mount -o loop,ro,offset=$((8192*512)) DietPi_v*_RPi-ARMv6-Buster.img mnt
 du -h -m --max-depth=0 mnt    #boot uncompressed_tarball_size
 cd mnt
 bsdtar --numeric-owner --format gnutar -cpf ../boot.tar .
@@ -18,25 +18,33 @@ cd .. && umount mnt
 xz -9 -e boot.tar
 
 # root tarball
-mount -o loop,ro,offset=$((98304*512)) DietPi_v*_RPi-ARMv6-Stretch.img mnt
+mount -o loop,ro,offset=$((540672*512)) DietPi_v*_RPi-ARMv6-Buster.img mnt
 du -h -m --max-depth=0 mnt    #root uncompressed_tarball_size
 cd mnt
 bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../root.tar .
 cd .. && umount mnt
 xz -9 -e root.tar
 
-sha512sum boot.tar.xz
-sha512sum root.tar.xz
+# Get total download size in bytes
+echo $(($(wc -c < boot.tar.xz) + $(wc -c < root.tar.xz)))   #os.json download_size
 
-# Upload tarballs
+sha512sum boot.tar.xz  #boot sha512sum
+sha512sum root.tar.xz  #root sha512sum
+
+# Backup old & Upload new tarballs
 sftp matthuisman@frs.sourceforge.net
 cd /home/frs/project/pinn-matthuisman/os/DietPi
+
+rm boot.tar.xz.bu
+rm root.tar.xz.bu
+
+rename boot.tar.xz  boot.tar.xz.bu
+rename root.tar.xz  root.tar.xz.bu
+
 put boot.tar.xz
 put root.tar.xz
-exit
 
-# Get total download size in bytes
-echo $(($(wc -c < boot.tar.xz) + $(wc -c < root.tar.xz)))
+exit
 
 # UPDATE os.json
 # UPDATE partitions.json
