f9c91c1994d95fa030241a90d4af941f3f4d9ebb
Matthew Huisman <gitlab@matthuisman.nz>
2021-08-29 09:03:29 +1200

    DietPi > v7.5.2

--- DietPi/HOW-TO.sh
+++ DietPi/HOW-TO.sh
@@ -3,14 +3,14 @@
 apt-get update && apt-get install -y p7zip bsdtar aria2
 
 cd ~ && mkdir mnt
-aria2c -x 4 -s 4  https://dietpi.com/downloads/images/DietPi_RPi-ARMv6-Buster.7z
-p7zip -d DietPi_RPi-ARMv6-Buster.7z
+aria2c -x 4 -s 4  https://dietpi.com/downloads/images/DietPi_RPi-ARMv6-Bullseye.7z
+p7zip -d DietPi_RPi-ARMv6-Bullseye.7z
 
-fdisk -l DietPi_v*_RPi-ARMv6-Buster.img
+fdisk -l DietPi_RPi-ARMv6-Bullseye.img
 # Start Sector * Sector Size = Below Offsets
 
 # boot tarball
-mount -o loop,ro,offset=$((8192*512)) DietPi_v*_RPi-ARMv6-Buster.img mnt
+mount -o loop,ro,offset=$((8192*512)) DietPi_RPi-ARMv6-Bullseye.img mnt
 du -h -m --max-depth=0 mnt    #boot uncompressed_tarball_size
 cd mnt
 bsdtar --numeric-owner --format gnutar -cpf ../boot.tar .
@@ -18,7 +18,7 @@ cd .. && umount mnt
 xz -T0 -9 -e boot.tar
 
 # root tarball
-mount -o loop,ro,offset=$((540672*512)) DietPi_v*_RPi-ARMv6-Buster.img mnt
+mount -o loop,ro,offset=$((270336*512)) DietPi_RPi-ARMv6-Bullseye.img mnt
 du -h -m --max-depth=0 mnt    #root uncompressed_tarball_size
 cd mnt
 bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../root.tar .
@@ -44,6 +44,7 @@ rename root.tar.xz  root.tar.xz.bu
 put boot.tar.xz
 put root.tar.xz
 
+exit
 exit
 
 # UPDATE os.json
