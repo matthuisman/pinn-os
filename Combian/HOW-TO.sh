fca342efe4587898f2dde770af6a9314e6967757
Matthew Huisman <mhuisman@enphaseenergy.com>
2023-01-12 16:08:09 +1300

    wip

--- Combian/HOW-TO.sh
+++ Combian/HOW-TO.sh
@@ -1,29 +1,26 @@
-# Debian 9 VPS 2 vCore 4096 MB Silicon Valley
+# Intel Regular Performance - Silicon Valley - Debian 10 - 2Core 4GB
 
-apt-get update
-apt-get install -y software-properties-common
-apt-add-repository non-free
-apt-get update
-
-apt-get install unrar bsdtar
+apt-get update && apt-get install -y p7zip bsdtar
 
 cd ~ && mkdir mnt
 
-export googleid=1sNhrr2u6n48vb5xuOe8P9pTayojQoOc_
+# 3.5.3
+export googleid=10HDC5CamECYUD160NRfv1IcFHxH-n_y8
 
 wget --save-cookies cookies.txt --keep-session-cookies --no-check-certificate \
     'https://docs.google.com/uc?export=download&id='$googleid -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > code.txt
 
-wget -O combian.rar --load-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$googleid'&confirm='$(<code.txt)
+wget -O combian.7z --load-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$googleid'&confirm='$(<code.txt)
 
-unrar x combian.rar
-rm cookies.txt code.txt combian.rar
+p7zip -d combian.7z
+mv Combian*.img Combian.img
+rm cookies.txt code.txt
 
-fdisk -l Combian*.img
+fdisk -l Combian.img
 # Start Sector * Sector Size = Below Offsets
 
 # boot tarball
-mount -o loop,ro,offset=$((16*512)) Combian*.img mnt
+mount -o loop,ro,offset=$((8192*512)) Combian.img mnt
 du -h -m --max-depth=0 mnt    #boot uncompressed_tarball_size
 cd mnt
 bsdtar --numeric-owner --format gnutar -cpf ../boot.tar .
@@ -31,7 +28,7 @@ cd .. && umount mnt
 xz -T0 -9 -e boot.tar
 
 # root tarball
-mount -o loop,ro,norecovery,offset=$((125056*512)) Combian*.img mnt
+mount -o loop,ro,norecovery,offset=$((532480*512)) Combian.img mnt
 du -h -m --max-depth=0 mnt     #root uncompressed_tarball_size
 cd mnt
 bsdtar --numeric-owner --format gnutar --one-file-system -cpf ../root.tar .
