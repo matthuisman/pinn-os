f09e35e6c7b834b54992cc49643a1758cf58f930
matthuisman <gitlab@matthuisman.nz>
2023-07-25 21:41:49 +1200

    add Rasplex pi3b+

---
+++ RasPlex_RPi3+/HOW-TO.sh
@@ -0,0 +1,32 @@
+# Debian 10 VPS 4 vCore 8GB Silicon Valley
+
+apt-get update && apt-get install -y unzip bsdtar
+
+cd ~ && mkdir mnt
+wget https://github.com/RasPlex/RasPlex/releases/download/1.8.0b/RasPlex-1.8.0.0b-573b6d73-RPi3bplus.arm.img.gz
+gzip -d RasPlex-*.img.gz
+
+fdisk -l RasPlex-*.img
+# Start Sector * Sector Size = Below Offsets
+
+# boot tarball
+mount -o loop,ro,offset=$((8192*512)) -t vfat RasPlex-*.img mnt
+du -h -m --max-depth=0 mnt    #boot uncompressed_tarball_size
+cd mnt
+bsdtar --numeric-owner --format gnutar -cpf ../boot.tar .
+cd .. && umount mnt
+xz -T0 -9 -e boot.tar
+
+# Upload tarball
+sftp matthuisman@frs.sourceforge.net
+cd /home/frs/project/pinn-matthuisman/os/RasPlex_RPi3+
+put boot.tar.xz
+exit
+
+# Get total download size in bytes
+echo $(wc -c < boot.tar.xz)
+
+sha512sum boot.tar.xz
+
+# UPDATE os.json
+# UPDATE partitions.json
---
+++ RasPlex_RPi3+/partition_setup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -x
+#supports_backup in PINN
+
+MOUNTPOINT="/tmp/RasPlex-System"
+
+md5sumCheck() {
+  ( cd $MOUNTPOINT
+    echo "checking MD5: $1"
+    md5sum -c $1.md5
+    if [ "$?" = "1" ]; then
+      echo "#######################################################"
+      echo "#                                                     #"
+      echo "#  RasPlex failed md5 check - Installation will quit  #"
+      echo "#                                                     #"
+      echo "#    Your original download was probably corrupt.     #"
+      echo "#   Please visit rasplex.com and get another copy     #"
+      echo "#                                                     #"
+      echo "#######################################################"
+      exit 1
+    fi
+    rm -rf $1.md5
+  )
+}
+
+if [ -z $part1 -o -z $part2 -o -z $id1 -o -z $id2 ]; then
+  echo "error: part1, part2, id1 or id2 not specified"
+  echo "actual values:"
+  echo "part1:" $part1
+  echo "part2:" $part2
+  echo "id1  :" $id1
+  echo "id2  :" $id2
+  exit 1
+fi
+
+# create mountpoint
+  mkdir -p $MOUNTPOINT
+
+# mount needed partition
+  mount $part1 $MOUNTPOINT
+
+# check md5sum
+  md5sumCheck kernel.img
+  md5sumCheck SYSTEM
+
+# create bootloader configuration
+  echo "creating bootloader configuration..."
+  echo "boot=$id1 disk=$id2 quiet" > $MOUNTPOINT/cmdline.txt
+
+# cleanup mountpoint
+  umount $MOUNTPOINT
+  rmdir $MOUNTPOINT
